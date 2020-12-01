7d4f81c6575c693bccad0df76914b9d87cdc7b9e
twyz <75316191+twyz@users.noreply.github.com>
2020-12-01 14:14:45 +0100

    Update ping-kill.bat

--- ping-kill.bat
+++ ping-kill.bat
@@ -1,4 +1,12 @@
 @echo off
+color a 
+title MPolymath's Pinger
+
+echo Hello ! Paste ur IP just here !
+set /p IP=Enter Website/IP:
+cls
+goto:ping
+
 :ping
 ping %ip% -n 1 -w 2000 || goto pingFail
 echo Ping was Successful to %ip% at %date% %time%
